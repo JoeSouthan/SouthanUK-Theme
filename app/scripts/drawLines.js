739bc52fe6f1da2a5260a76ded25c9ca1d07f525
Joseph Southan <joseph@southanuk.co.uk>
2014-04-24 02:19:58 +0100

    Moved to Yeoman and Grunt

--- app/scripts/drawLines.js
+++ app/scripts/drawLines.js
@@ -5,15 +5,28 @@ jQuery(document).ready(function($) {
         posts   = $("div[type^=post]"),
         coords  = {};
 
-    window.addEventListener('resize', resizeCanvas, false);
+    //Fire these on page load
+    resizeCanvas();
+    drawHandler();
 
+
+    //Listeners 
+    $(window).on('resize', function() {
+        resizeCanvas();
+    });
     $(window).on('scroll',function(){
         drawHandler();
+        focusPost();
+        isTopPost();
     });
     $(document).on('webkitTransitionEnd oTransitionEnd transitionend msTransitionEnd', function() {
         drawHandler();
     });
     
+
+    //Functions - Lines
+
+    //Find Coordinates for each post div
     function findC () {
         $.each(sPosts, function(key , val) {
             var windowOff = Math.round($(window).scrollTop());
@@ -26,11 +39,14 @@ jQuery(document).ready(function($) {
         });
     }
 
+    //Resize the page canvas
     function resizeCanvas() {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
         drawHandler();
     }
+
+    //Handles the drawing of the post lines
     function drawHandler () {
         findC();
         canvas.width = canvas.width;
@@ -39,6 +55,7 @@ jQuery(document).ready(function($) {
         });
     }
 
+    //Draws the post lines
     function _drawPostLines(originX, originY, endX, endY, selector) {
         context.beginPath();
         context.lineWidth = "1";
@@ -52,6 +69,29 @@ jQuery(document).ready(function($) {
         context.lineTo(endX,endY);
         context.stroke();
     }
-    resizeCanvas();
-    drawHandler();
+    //Posts
+
+    //Focuses the in view post
+    function focusPost () {
+        $.each(coords, function(index, val) {
+            if (!isInView($("#"+index))){
+                $("#"+index).stop().animate({opacity: 0.4},500);
+            } else {
+                $("#"+index).stop().animate({opacity: 1},200);
+            }
+        });
+    }
+    function isInView(elem){
+        var windowOff = $(window).scrollTop();
+        var windowSize = windowOff + $(window).height()-200;
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height()-200;
+
+        return ((elemBottom >= windowOff) && (elemTop <= windowSize));
+    }
+    function isTopPost () {
+
+    }
+
 });
