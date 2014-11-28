aee38fe8577c7a3b182220bcedd4dc9efc324ea1
Joe Southan <joseph@southanuk.co.uk>
2014-11-28 23:14:18 +0000

    Changing theme around [WIP]

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -1,3 +1,7 @@
 jQuery(document).ready(function() {
-    
+  $(document).on('scroll', function(event) {
+    var headerOffset = -$("#img-header").offset().top / 2 | 0;
+    headerOffset = headerOffset > 0 ? 0 : headerOffset;
+    $("#img-header").css('top', headerOffset);
+  });
 });
