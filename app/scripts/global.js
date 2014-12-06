70df7ea03a789ef5ebc9eec9f33f3e0ea6f0f6f9
Joe Southan <joseph@southanuk.co.uk>
2014-12-06 01:36:38 +0000

    Major change in theme, moving towards Ghost migration/hbs theme

--- app/scripts/global.js
+++ app/scripts/global.js
@@ -1,7 +1,27 @@
+// http://upshots.org/javascript/jquery-test-if-element-is-in-viewport-visible-on-screen
+$.fn.isOnScreen = function(){
+    var viewport = {};
+    viewport.top = $(window).scrollTop();
+    viewport.bottom = viewport.top + $(window).height();
+    var bounds = {};
+    bounds.top = this.offset().top;
+    bounds.bottom = bounds.top + this.outerHeight();
+    return ((bounds.top <= viewport.bottom) && (bounds.bottom >= viewport.top));
+};
+
 jQuery(document).ready(function() {
   $(document).on('scroll', function(event) {
+    // Check the image header
     var headerOffset = -$("#img-header").offset().top / 2 | 0;
     headerOffset = headerOffset > 0 ? 0 : headerOffset;
     $("#img-header").css('top', headerOffset);
+
+    // Move the logo on scroll
+    var logo = '<img src="images/smalllogo.png" class="img-responsive" alt="Logo" id="logo-sidebar" />';
+    if ($("#logo").isOnScreen()) {
+      $("#logo-sidebar").length ? $("#logo-sidebar").slideUp(300, function() {$(this).remove()}) : null;
+    } else {
+      $("#logo-sidebar").length ? null : $(logo).prependTo('#sidebar-wrapper').hide().slideDown(300);
+    }
   });
 });
--- readme.md
+++ readme.md
@@ -1,22 +1,32 @@
 # SouthanUK Theme
-A theme for my website [SouthanUK](http://www.southanuk.co.uk) which will *eventually* become a WordPress theme.
+A theme for my website [SouthanUK](http://www.southanuk.co.uk) which ~~will *eventually* become a WordPress theme~~ Ghost Theme.
 
 ###Uses :
  - Yeoman
  - Grunt
- - LESS
- - Bootstrap
+ - SASS
+ - Bootstrap 3
  - jQuery
 
-###Installation :
+###How to run :
 ```
 npm install
 bower install
-grunt --force #(For the time being ;) )
+grunt serve 
 ```
-Compiled site in dist/
+~~Compiled site in dist/~~ ```grunt dist``` has problems at this moment
 
 ###Avaliable grunt flags :
  - serve - Starts the development enviroment server 
  - serve:dist - Compiles site for production then starts a server
- - test - Runs tests *(Not complete)*
+
+
+###TODO :
+  - [ ] Refactor the CSS (It's a mess!)
+  - [ ] Convert to .hbs
+  - [ ] Finalise colours
+  - [ ] Remove unnecessary npm packages
+  - [ ] Trim Gruntfile.js
+  - [ ] Background on the left sidebar? Something space?
+  - [ ] HTML5 it 
+  - [ ] Remove right sidebar?
